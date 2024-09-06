e1d960629a11ed91d356c7af2f38501de42c3520
Nghi Nguyen Thanh <nghi.nguyen@evehr.vn>
2024-09-06 20:46:39 +0700

    feat: task 10 - task 13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,3 +1,4 @@
+const axios = require("axios").default;
 const express = require("express");
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
@@ -28,54 +29,95 @@ public_users.post("/register", (req, res) => {
   res.status(201).send(`${username} created!`);
 });
 
+axios.get = async (url) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (url === "./booksdb.js") {
+        resolve({ data: books });
+      } else {
+        reject(new Error("Not found"));
+      }
+    }, 150);
+  });
+};
+
 // Get the book list available in the shop
-public_users.get("/", function (req, res) {
+public_users.get("/", async function (req, res) {
   //Write your code here
-  return res.status(200).send(JSON.stringify(books, null, 4));
+  try {
+    const resp = await axios.get("./booksdb.js");
+    const booksdb = resp.data;
+    const bookList = Object.values(booksdb);
+    return res.status(200).send(JSON.stringify(bookList, null, 4));
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 // Get book details based on ISBN
-public_users.get("/isbn/:isbn", function (req, res) {
+public_users.get("/isbn/:isbn", async function (req, res) {
   //Write your code here
   const { params } = req || {};
   if (params?.isbn) {
-    const currentBook = books[params?.isbn];
-    if (currentBook) {
-      return res.status(200).send(JSON.stringify(currentBook, null, 4));
+    try {
+      const resp = await axios.get("./booksdb.js");
+      const booksdb = resp.data;
+      const currentBook = booksdb[params?.isbn];
+      if (currentBook) {
+        return res.status(200).send(JSON.stringify(currentBook, null, 4));
+      }
+      return res.status(404).json({ message: "Book not found!" });
+    } catch (error) {
+      return res.status(500).json({ message: "Internal Server Error" });
     }
-    return res.status(404).json({ message: "Book not found!" });
   }
 
   return res.status(404).json({ message: "ISBN not valid!" });
 });
 
 // Get book details based on author
-public_users.get("/author/:author", function (req, res) {
+public_users.get("/author/:author", async function (req, res) {
   //Write your code here
   const { params } = req || {};
   if (params?.author) {
-    const currentAuthorBooks = bookList.filter(
-      (book) => book.author === params?.author
-    );
-    if (currentAuthorBooks) {
-      return res.status(200).send(JSON.stringify(currentAuthorBooks, null, 4));
+    try {
+      const resp = await axios.get("./booksdb.js");
+      const booksdb = resp.data;
+      const bookList = Object.values(booksdb);
+      const currentAuthorBooks = bookList.filter(
+        (book) => book.author === params?.author
+      );
+      if (currentAuthorBooks?.length > 0) {
+        return res
+          .status(200)
+          .send(JSON.stringify(currentAuthorBooks, null, 4));
+      }
+      return res.status(404).json({ message: "Author not found!" });
+    } catch (error) {
+      return res.status(500).json({ message: "Internal Server Error" });
     }
-    return res.status(404).json({ message: "Author not found!" });
   }
 
   return res.status(404).json({ message: "Author not valid!" });
 });
 
 // Get all books based on title
-public_users.get("/title/:title", function (req, res) {
+public_users.get("/title/:title", async function (req, res) {
   //Write your code here
   const { params } = req || {};
   if (params?.title) {
-    const currentBook = bookList.find((book) => book.title === params?.title);
-    if (currentBook) {
-      return res.status(200).send(JSON.stringify(currentBook, null, 4));
+    try {
+      const resp = await axios.get("./booksdb.js");
+      const booksdb = resp.data;
+      const bookList = Object.values(booksdb);
+      const currentBook = bookList.find((book) => book.title === params?.title);
+      if (currentBook) {
+        return res.status(200).send(JSON.stringify(currentBook, null, 4));
+      }
+      return res.status(404).json({ message: "Title not found!" });
+    } catch (error) {
+      return res.status(500).json({ message: "Internal Server Error" });
     }
-    return res.status(404).json({ message: "Title not found!" });
   }
 
   return res.status(404).json({ message: "Title not valid!" });
